af697dd3993dfa8bf8b193ab4177a613140c2834
Conrado <42232126+bojorquezc@users.noreply.github.com>
2024-08-18 21:19:23 -0700

    First component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Greeting from './Greeting.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Greeting />
+  </React.StrictMode>,
 )
